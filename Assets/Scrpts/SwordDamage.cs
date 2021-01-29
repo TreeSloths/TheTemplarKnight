c96fbd5ea75de683421430e6fab4cadccfa31de3
EmilHuzell <65963728+EmilHuzell@users.noreply.github.com>
2021-01-29 21:34:58 +0100

    Creates player prefab
    creates a player prefab with simple movement script and a sword that can register collissions

---
+++ Assets/Scrpts/SwordDamage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordDamage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision collision)
+    {
+        // Debug-draw all contact points and normals
+        if(collision.gameObject.name == "Box")
+            Destroy(GameObject.Find("Box"));
+    }
+}
