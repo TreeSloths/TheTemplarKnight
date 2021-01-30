7b04804e6fc006a398df0552b981a795d3dea729
EmilHuzell <65963728+EmilHuzell@users.noreply.github.com>
2021-01-31 00:44:25 +0100

    Adds character prefab to player gameobject and makes controls for animations

--- Assets/Scrpts/PlayerMovement.cs
+++ Assets/Scrpts/PlayerMovement.cs
@@ -4,24 +4,50 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public int Speed;
+    //private D
+    private bool IsClipPlaying(string name)
+    {
+        var animationController = gameObject.transform.Find("arthur_01").GetComponent<Animator>();
+
+        foreach(var clipInfo in animationController.GetCurrentAnimatorClipInfo(0))
+        {
+            Debug.Log(clipInfo.clip.ToString());
+            if(clipInfo.clip.ToString() == name)
+            {
+                return true;
+            }
+            //Debug.Log(clipInfo.clip.ToString());
+
+        }
+        return false;
+    }
     void Update()
     {
-        if(Input.GetKey("w"))
+        var animationController =  gameObject.transform.Find("arthur_01").GetComponent<Animator>();
+        //IsClipPlaying();
+        //Debug.Log(IsClipPlaying("arthur_idle_01 (UnityEngine.AnimationClip)"));
+        if(Input.GetKey("w") && IsClipPlaying("arthur_walking_01 (UnityEngine.AnimationClip)") == false)
         {
-            GetComponent<Rigidbody>().velocity += transform.forward;
+            animationController.Play("arthur_walk_01");
+            Debug.Log("hello");
+            
         }
-        if(Input.GetKey("a"))
+       /* else if()
         {
-            GetComponent<Rigidbody>().velocity -= transform.right;
+           animationController.Play("arthur_idle_01"); 
+        }*/
+        
+        /*if(Input.GetKey("a"))
+        {
+            
         }
         if(Input.GetKey("s"))
         {
-            GetComponent<Rigidbody>().velocity -= transform.forward;
+            
         }
         if(Input.GetKey("d"))
         {
-            GetComponent<Rigidbody>().velocity += transform.right;
-        }
+            
+        }*/
     }
 }
