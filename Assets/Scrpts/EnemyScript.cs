7479a2cc2e56a8e48b69cbb90e58edf1daf0a752
Adam Alzuhairi <adam_alzu@hotmail.com>
2021-01-30 10:01:28 +0100

    Created and programmed EnemyScript and Playerstats

---
+++ Assets/Scrpts/EnemyScript.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public int MoveSpeed = 4;
+    public int MaxDist = 10;
+    public int MinDist = 5;
+    private Playerstats Playerstats;
+    public GameObject player;
+
+    void Start()
+    {
+        Playerstats = FindObjectOfType<Playerstats>();
+    }
+
+
+    void Update()
+    {
+        var Player = GameObject.FindWithTag("Player");
+        var playerPos = Player.transform.position;
+        transform.LookAt(playerPos);
+
+        if (Vector3.Distance(transform.position, playerPos) >= MinDist)
+        {
+            transform.position += transform.forward * (MoveSpeed * Time.deltaTime);
+
+
+            if (Vector3.Distance(transform.position, playerPos) <= MaxDist)
+            {
+                Playerstats.playerHealth -= 5;
+            }
+        }
+    }
+}
---
+++ Assets/Scrpts/Playerstats.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerstats : MonoBehaviour
+{
+    public int playerHealth;
+
+    private void Start()
+    {
+        playerHealth = 100;
+    }
+
+    private void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
