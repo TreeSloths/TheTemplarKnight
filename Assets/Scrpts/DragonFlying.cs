3c54a8f71e34a5addd8cea63577000011e0b141d
EmilHuzell <65963728+EmilHuzell@users.noreply.github.com>
2021-01-31 12:21:48 +0100

    Adds dragon flying script

---
+++ Assets/Scrpts/DragonFlying.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonFlying : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int turnInterval;
+    public int i;
+    public float dragonspeed;
+    void Update()
+    {
+        i++;
+        if(i >= turnInterval)
+        {
+            transform.Rotate(0,90,0);
+            i=0;
+        }
+        transform.position += transform.forward * dragonspeed;
+    }
+}
